54d0eac48d8c3199fa79bd097b1c98df830c7826
akihiro99 <102530779+akihiro99@users.noreply.github.com>
2025-07-31 13:53:33 +0800

    Add files via upload

---
+++ .github/workflows/src/pages/RESUME.e4mcl.js
@@ -0,0 +1,16 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+	// Write your Javascript code here using the Velo framework API
+
+	// Print hello world:
+	// console.log("Hello world!");
+
+	// Call functions on page elements, e.g.:
+	// $w("#button1").label = "Click me!";
+
+	// Click "Run", or Preview your site, to execute your code
+
+<a href="https://academy.yoast.com/certificate/2558/1835718/9302672c4e78d83525e979e643010d84/1/" target="_blank"><img src="https://academy.yoast.com/badge/2558/1835718/9302672c4e78d83525e979e643010d84/1/" width="200" alt=" successfully completed the SEO for beginners course!"/></a>
+});
